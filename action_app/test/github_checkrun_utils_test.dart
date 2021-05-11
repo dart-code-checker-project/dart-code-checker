1a63c4197ab8d8eb5f735d37298d6ff95bdd0e89
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-05-11 23:51:42 +0300

    feat: introduce GitHub check run util issueToAnnotation

--- action_app/test/github_checkrun_utils_test.dart
+++ action_app/test/github_checkrun_utils_test.dart
@@ -1,9 +1,12 @@
 @TestOn('vm')
+import 'package:dart_code_metrics/src/analyzers/models/issue.dart' as dcm;
+import 'package:dart_code_metrics/src/analyzers/models/replacement.dart' as dcm;
 import 'package:dart_code_metrics/src/analyzers/models/severity.dart' as dcm;
 import 'package:dart_code_metrics_github_action_app/src/github_checkrun_utils.dart';
 import 'package:dart_code_metrics_github_action_app/src/github_workflow_utils.dart';
 import 'package:github/github.dart' as github;
 import 'package:mocktail/mocktail.dart';
+import 'package:source_span/source_span.dart';
 import 'package:test/test.dart';
 
 class GitHubWorkflowUtilsMock extends Mock implements GitHubWorkflowUtils {}
@@ -14,6 +17,104 @@ void main() {
 
     setUp(() {
       workflowUtilsMock = GitHubWorkflowUtilsMock();
+    });
+
+    group('issueToAnnotation returns', () {
+      const absolutePath = '/home/developer/project/source.dart';
+
+      setUp(() {
+        when(workflowUtilsMock.currentPathToRepoRoot)
+            .thenReturn('/home/developer/project');
+      });
+
+      test('github annotation from dart_code_metrics multi line issue', () {
+        final utils = GitHubCheckRunUtils(workflowUtilsMock);
+
+        final issue = dcm.Issue(
+          ruleId: 'rule',
+          documentation: Uri(),
+          location: SourceSpan(
+            SourceLocation(
+              1,
+              sourceUrl: Uri.parse('file://$absolutePath'),
+              line: 1,
+              column: 2,
+            ),
+            SourceLocation(
+              1,
+              sourceUrl: Uri.parse('file://$absolutePath'),
+              line: 3,
+              column: 4,
+            ),
+            '',
+          ),
+          severity: dcm.Severity.none,
+          message: 'message',
+        );
+
+        final annotation = utils.issueToAnnotation(absolutePath, issue);
+
+        expect(annotation.path, equals('source.dart'));
+        expect(annotation.startLine, equals(1));
+        expect(annotation.endLine, equals(3));
+        expect(annotation.startColumn, isNull);
+        expect(annotation.endColumn, isNull);
+        expect(
+          annotation.annotationLevel,
+          equals(github.CheckRunAnnotationLevel.notice),
+        );
+        expect(annotation.message, equals('message'));
+        expect(annotation.title, isEmpty);
+        expect(annotation.rawDetails, isNull);
+      });
+      test('github annotation from dart_code_metrics single line issue', () {
+        final utils = GitHubCheckRunUtils(workflowUtilsMock);
+
+        final issue = dcm.Issue(
+          ruleId: 'rule',
+          documentation: Uri(),
+          location: SourceSpan(
+            SourceLocation(
+              1,
+              sourceUrl: Uri.parse('file://$absolutePath'),
+              line: 1,
+              column: 2,
+            ),
+            SourceLocation(
+              1,
+              sourceUrl: Uri.parse('file://$absolutePath'),
+              line: 1,
+              column: 4,
+            ),
+            '',
+          ),
+          severity: dcm.Severity.none,
+          message: 'message',
+          verboseMessage: 'verbose message',
+          suggestion: const dcm.Replacement(
+            comment: 'replacement comment',
+            replacement: 'new code',
+          ),
+        );
+
+        final annotation = utils.issueToAnnotation(absolutePath, issue);
+
+        expect(annotation.path, equals('source.dart'));
+        expect(annotation.startLine, equals(1));
+        expect(annotation.endLine, equals(1));
+        expect(annotation.startColumn, equals(2));
+        expect(annotation.endColumn, equals(4));
+        expect(
+          annotation.annotationLevel,
+          equals(github.CheckRunAnnotationLevel.notice),
+        );
+        expect(
+          annotation.message,
+          equals('verbose message\nreplacement comment'),
+        );
+        expect(annotation.title, equals('message'));
+        expect(annotation.rawDetails, isNull);
+      });
     });
 
     group('severityToAnnotationLevel returns', () {
