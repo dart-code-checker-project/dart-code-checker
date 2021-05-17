01b10f5bc3b84dd4d8a9a82b4249ded2c2a0a78c
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-05-18 00:22:59 +0300

    test: GitHubWorkflowUtils currentRepositorySlug cover by tests

--- action_app/test/github_workflow_utils_test.dart
+++ action_app/test/github_workflow_utils_test.dart
@@ -95,6 +95,26 @@ void main() {
       });
     });
 
+    test('currentRepositorySlug returns defined slug of the repository', () {
+      expect(
+        () {
+          GitHubWorkflowUtils(environmentVariables: {}, output: output)
+              .currentRepositorySlug();
+        },
+        throwsArgumentError,
+      );
+
+      const slug = 'dart-code-checker/run-dart-code-metrics-action';
+
+      expect(
+        GitHubWorkflowUtils(
+          environmentVariables: {'GITHUB_REPOSITORY': slug},
+          output: output,
+        ).currentRepositorySlug(),
+        equals(slug),
+      );
+    });
+
     test('logDebugMessage logs passed message', () {
       const message = 'simple message';
       const path = '/project/lib/source.dart';
